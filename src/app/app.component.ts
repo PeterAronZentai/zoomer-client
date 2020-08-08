b2848a3bda54b66cbab2f2e57bb39a9bc1aabdc1
pz-gs <peter.zentai@guidesmiths.com>
2020-08-08 23:09:10 +0100

    initial

---
+++ src/app/app.component.ts
@@ -0,0 +1,93 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DOCUMENT } from '@angular/common';
+
+import { ZoomMtg } from '@zoomus/websdk';
+
+ZoomMtg.preLoadWasm();
+ZoomMtg.prepareJssdk();
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  // setup your signature endpoint here: https://github.com/zoom/websdk-sample-signature-node.js
+  signatureEndpoint = 'http://localhost:4000'
+  apiKey = 'HhgYnT6QTwmdNRBGjwedfQ'
+  meetingNumber = 99801406568
+  role = 1
+  leaveUrl = 'http://localhost:4200'
+  userName = 'Angular'
+  userEmail = 'zed@was.here.com'
+  passWord = '424136'
+
+  constructor(public httpClient: HttpClient, @Inject(DOCUMENT) document) {
+
+  }
+
+  ngOnInit() {
+
+  }
+
+  getSignature() {
+    this.httpClient.post(this.signatureEndpoint, {
+	    meetingNumber: this.meetingNumber,
+	    role: this.role
+    }).toPromise().then((data: any) => {
+      if(data.signature) {
+        console.log(data.signature)
+        this.startMeeting(data.signature)
+      } else {
+        console.log(data)
+      }
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
+
+  startMeeting(signature) {
+
+    // alert('!')
+    document.getElementById('zmmtg-root').style.display = 'block'
+
+    // const container = document.getElementById('zmmtg-root');
+    // alert(container)
+    // const newc = document.createElement('div');
+    // newc.setAttribute("data-zed", "x"); 
+    // document.body.removeChild(container);
+    // newc.appendChild(container);
+    // document.body.appendChild(newc);
+
+    // return;
+    ZoomMtg.init({
+      leaveUrl: this.leaveUrl,
+
+      isSupportAV: true,
+      success: (success) => {
+        console.log(success)
+
+        ZoomMtg.join({
+          signature: signature,
+          meetingNumber: this.meetingNumber,
+          userName: this.userName,
+          apiKey: this.apiKey,
+          userEmail: this.userEmail,
+          passWord: this.passWord,
+          success: (success) => {
+            console.log(success)
+          },
+          error: (error) => {
+            console.log(error)
+          }
+        })
+
+      },
+      error: (error) => {
+        console.log(error)
+      }
+    })
+  }
+}
